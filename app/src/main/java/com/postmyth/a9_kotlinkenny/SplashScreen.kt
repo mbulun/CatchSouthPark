06125e4c9a403b7043e05bb3fd3d6b91946a9e5c
Your Name <you@example.com>
2022-06-05 22:31:58 +0300

    onuncu gonderim google playe gonderildi. İnceleme sürüyor. İnceleme red yemezse koda tekrar dokunmayacağım

--- app/src/main/java/com/postmyth/a9_kotlinkenny/SplashScreen.kt
+++ app/src/main/java/com/postmyth/a9_kotlinkenny/SplashScreen.kt
@@ -18,6 +18,8 @@ val networkControl = NetworkControl()
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        println(getResources().getString(R.string.app_name))
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
